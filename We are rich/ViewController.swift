6b04ded8e7efa93a0fa2c0efba14d2b7e69bbf0c
Artem <kuzminykh.artem@gmail.com>
2019-12-10 23:47:00 +0200

    The we are rich 1

---
+++ We are rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  We are rich
+//
+//  Created by Artem on 30.11.2019.
+//  Copyright © 2019 Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
